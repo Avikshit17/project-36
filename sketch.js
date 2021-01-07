bf2733d429877acb7ec14a6cea9a4d7852636a22
Avikshit17 <71918450+Avikshit17@users.noreply.github.com>
2021-01-07 11:41:31 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,73 @@
+//Create variables here
+var dog,happyDog,foods,foodStock,database,dogImage,happyDogImage
+
+function preload()
+{
+  //load images here
+  dogImage=loadImage("dog.png")
+  happyDogImage=loadImage("happydog.png")
+  milkImage=loadImage("Milk.png")
+}
+
+function setup() {
+  createCanvas(500, 500);
+  dog=createSprite(250,250,50,50);
+  dog.addImage(dogImage)
+  dog.scale=0.1
+  database=firebase.database()
+  foodStock=database.ref('food');
+  foodStock.on("value",readStock);
+}
+
+
+function draw() {  
+  background(46,139,87)
+  display()
+  stroke ("black")
+  text("food remaining:"+foods,250,50)
+  if(keyWentDown(UP_ARROW)){
+    writeStock(foods);
+    dog.addImage(happyDogImage);
+  }
+  if(keyWentUp(UP_ARROW)){
+    dog.addImage(dogImage)
+  }
+
+  drawSprites();
+  //add styles here
+  
+
+}
+function readStock(data)
+{
+  foods=data.val();
+
+}
+function writeStock(x)
+{
+  if(x<=0){
+    x=0;
+
+  }else{
+    x=x-1;
+  }
+
+  database.ref('/').update({
+food:x
+  })
+}
+function display()
+{
+  var x=50
+  for(i=1;i<=foods;i=i+1)
+  {
+  image(milkImage,x,50,30,30)
+  x=x+10
+  }
+}
+
+
+
+
+
+
